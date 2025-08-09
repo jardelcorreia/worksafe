1228a2d8a8fc8d0f2a473db4aff4aa93047c9674
Jardel Correia <jardel.lc@gmail.com>
2025-08-09 16:26:34 +0000

    Error: Hydration failed because the server rendered HTML didn't match th

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,22 +3,24 @@ import * as React from 'react';
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean>(false);
-  const [hasMounted, setHasMounted] = React.useState(false);
+  const [isMobile, setIsMobile] = React.useState(false);
 
   React.useEffect(() => {
-    setHasMounted(true);
     const checkIsMobile = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
 
-    // Check on initial client render and add listener
+    // Initial check after mount
     checkIsMobile();
+
+    // Listen for resize events
     window.addEventListener('resize', checkIsMobile);
 
     // Cleanup listener on component unmount
-    return () => window.removeEventListener('resize', checkIsMobile);
+    return () => {
+      window.removeEventListener('resize', checkIsMobile);
+    };
   }, []);
 
-  return hasMounted ? isMobile : false;
+  return isMobile;
 }
