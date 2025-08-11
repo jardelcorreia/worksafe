664c4ddd0ffb0c97dfb5586884007832d6644f8d
Jardel Correia <jardel.lc@gmail.com>
2025-08-11 13:05:13 +0000

    vamos aumentar o tamanho da logo worksafe

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,10 +4,10 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
   return (
     <div className="flex items-center justify-center gap-2 group-data-[collapsible=icon]:h-12 group-data-[collapsible=icon]:w-12 group-data-[collapsible=icon]:p-2">
       <ShieldCheck
-        className="size-8 transition-all group-data-[collapsible=icon]:size-8"
+        className="size-10 transition-all group-data-[collapsible=icon]:size-8"
         strokeWidth={1.5}
       />
-      <div className="text-lg font-bold transition-all group-data-[collapsible=icon]:hidden">
+      <div className="text-2xl font-bold transition-all group-data-[collapsible=icon]:hidden">
         <span className="font-headline">WorkSafe</span>
       </div>
     </div>
