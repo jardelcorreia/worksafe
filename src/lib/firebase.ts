8912d853bb49711343a06ae15de9c368e8fb8017
Jardel Correia <jardel.lc@gmail.com>
2025-08-15 14:43:48 +0000

    há uma chave de api exposta no firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig: FirebaseOptions = {
-  projectId: "worksafe-ai-kogel",
-  appId: "1:256398808652:web:81823c9c53440b56dd06e9",
-  storageBucket: "worksafe-ai-kogel.firebasestorage.app",
-  apiKey: "AIzaSyBgMgave8JnoDDWcV3x5RibHKUO2RinEj4",
-  authDomain: "worksafe-ai-kogel.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "256398808652"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
